2a1f67ae30941383c89d4dcafeb696f9b79055ab
jonatascastro12 <jonatascastro12@gmail.com>
2015-06-18 17:40:59 -0300

    removed python compiled files

---
+++ main/dashboard_reports.py
@@ -0,0 +1,22 @@
+from django.utils.translation import gettext as _
+from dashboard_view.dashboard_reports import DashboardReport
+from main.forms import MonthBirthdayReportForm
+from main.models import UserProfile
+
+class MonthBirthdayReport(DashboardReport):
+    permission = ('can_view_userprofile')
+    model = UserProfile
+    filter_form = MonthBirthdayReportForm
+    list_display = ['user__get_full_name', 'birth_date__month', ]
+    actions = ['print', ]
+    icon = 'fa-birthday-cake'
+
+    def get_queryset(self):
+        objects = self.report.model.accounted.\
+            filter(birth_date__month=self.filter_form.cleaned_data.get('month')).\
+            all()
+        objects = list(objects)
+        objects = sorted(objects, key=lambda u:int(u.birth_date.day))
+
+        return objects
+
